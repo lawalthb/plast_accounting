3da744a1b4c9d4024433a34549689f08177c2d34
lawalthb <lawalthb@gmail.com>
2023-01-26 07:54:30 +0100

    all ledgers and products modules are done

--- app/Helpers/Menu.php
+++ app/Helpers/Menu.php
@@ -13,7 +13,8 @@
 		[
 			'path' => '',
 			'label' => __('customers'), 
-			'icon' => '<i class="material-icons ">contacts</i>','submenu' => [
+			'icon' => '<i class="material-icons ">contacts</i>',
+'submenu' => [
 		[
 			'path' => 'ledgers/customer_add',
 			'label' => __('createNew'), 
@@ -43,7 +44,8 @@
 		[
 			'path' => '',
 			'label' => __('suppliers'), 
-			'icon' => '<i class="material-icons ">person_add</i>','submenu' => [
+			'icon' => '<i class="material-icons ">person_add</i>',
+'submenu' => [
 		[
 			'path' => 'ledgers/suppliers_add',
 			'label' => __('createNew'), 
@@ -67,7 +69,8 @@
 		[
 			'path' => '',
 			'label' => __('allLedgers'), 
-			'icon' => '<i class="material-icons ">local_library</i>','submenu' => [
+			'icon' => '<i class="material-icons ">local_library</i>',
+'submenu' => [
 		[
 			'path' => 'ledgers/any_add',
 			'label' => __('createNew'), 
@@ -91,7 +94,8 @@
 		[
 			'path' => '',
 			'label' => __('accountEntries'), 
-			'icon' => '<i class="material-icons ">chrome_reader_mode</i>','submenu' => [
+			'icon' => '<i class="material-icons ">chrome_reader_mode</i>',
+'submenu' => [
 		[
 			'path' => '',
 			'label' => __('contralVoucher'), 
@@ -149,9 +153,47 @@
 		],
 		
 		[
+			'path' => '',
+			'label' => __('products'), 
+			'icon' => '<i class="material-icons ">settings_input_composite</i>',
+'submenu' => [
+		[
+			'path' => 'products/product_add1',
+			'label' => __('createNew'), 
+			'icon' => '<i class="material-icons ">subdirectory_arrow_right</i>'
+		],
+		
+		[
+			'path' => 'products/products_list',
+			'label' => __('management'), 
+			'icon' => '<i class="material-icons ">subdirectory_arrow_right</i>'
+		],
+		
+		[
+			'path' => 'units/units_list',
+			'label' => __('units'), 
+			'icon' => '<i class="material-icons ">subdirectory_arrow_right</i>'
+		],
+		
+		[
+			'path' => 'product_categories/categories_list',
+			'label' => __('categories'), 
+			'icon' => '<i class="material-icons ">subdirectory_arrow_right</i>'
+		],
+		
+		[
+			'path' => '',
+			'label' => __('locations'), 
+			'icon' => '<i class="material-icons ">subdirectory_arrow_right</i>'
+		]
+	]
+		],
+		
+		[
 			'path' => 'menu23',
 			'label' => __('inventoryEntries'), 
-			'icon' => '<i class="material-icons ">collections</i>','submenu' => [
+			'icon' => '<i class="material-icons ">collections</i>',
+'submenu' => [
 		[
 			'path' => 'menu23',
 			'label' => __('stockJornal'), 
@@ -205,7 +247,8 @@
 		[
 			'path' => '',
 			'label' => __('payroll'), 
-			'icon' => '<i class="material-icons ">perm_contact_calendar</i>','submenu' => [
+			'icon' => '<i class="material-icons ">perm_contact_calendar</i>',
+'submenu' => [
 		[
 			'path' => '',
 			'label' => __('employees'), 
@@ -259,7 +302,8 @@
 		[
 			'path' => '',
 			'label' => __('insurance'), 
-			'icon' => '<i class="material-icons ">security</i>','submenu' => [
+			'icon' => '<i class="material-icons ">security</i>',
+'submenu' => [
 		[
 			'path' => '',
 			'label' => __('goodsInTransit'), 
@@ -277,7 +321,8 @@
 		[
 			'path' => '',
 			'label' => __('reports'), 
-			'icon' => '<i class="material-icons ">assessment</i>','submenu' => [
+			'icon' => '<i class="material-icons ">assessment</i>',
+'submenu' => [
 		[
 			'path' => '',
 			'label' => __('dayBook'), 
@@ -337,7 +382,8 @@
 		[
 			'path' => '',
 			'label' => __('settings'), 
-			'icon' => '<i class="material-icons ">settings</i>','submenu' => [
+			'icon' => '<i class="material-icons ">settings</i>',
+'submenu' => [
 		[
 			'path' => 'companies/user_list',
 			'label' => __('company'), 
@@ -355,7 +401,8 @@
 		[
 			'path' => '',
 			'label' => __('manageAdmins'), 
-			'icon' => '<i class="material-icons ">group</i>','submenu' => [
+			'icon' => '<i class="material-icons ">group</i>',
+'submenu' => [
 		[
 			'path' => '/users/usersincomp',
 			'label' => __('adminList'), 
@@ -393,6 +440,18 @@
 		],
 		[
 			'value' => 'No', 
+			'label' => __('no'), 
+		],] ;
+	}
+	
+	public static function status(){
+		return [
+		[
+			'value' => '1', 
+			'label' => __('yes'), 
+		],
+		[
+			'value' => '0', 
 			'label' => __('no'), 
 		],] ;
 	}
